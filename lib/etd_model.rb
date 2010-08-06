b58bc85339b1c83a16cdededd4d61ccd07f3166a
Douglas Kim <dougkim@stanford.edu>
2010-08-06 15:18:47 -0700

    Removed global kernel constants to utilize the gem in the hydra_etd application.

--- lib/etd_model.rb
+++ lib/etd_model.rb
@@ -1,9 +1,4 @@
 
 require 'rubygems'
 require 'active_fedora'
-require 'etd_model/etd'
-require 'etd_model/part'
-
-Kernel.const_set( 'Etd', EtdModel::Etd )
-Kernel.const_set( 'Part', EtdModel::Part )
 
