8d3e1426e01ef3b67cd581a1a0887343052007a6
Joe Atzberger <ohiocore@gmail.com>
2016-01-21 12:00:08 -0800

    Use a before block

--- spec/etd_model/etd_spec.rb
+++ spec/etd_model/etd_spec.rb
@@ -1,33 +1,30 @@
 require 'spec_helper'
 
 describe EtdModel::Etd do
-  it 'should respond to #get_embargo_date' do
+  before :each do
     @etd = EtdModel::Etd.new
+  end
+
+  it 'should respond to #get_embargo_date' do
     expect(@etd).to respond_to(:get_embargo_date)
   end
 
   it 'handles the Term element' do
-    @etd = EtdModel::Etd.new
-
     props_ds = @etd.datastreams['properties']
     expect(props_ds).to respond_to(:term_values)
   end
 
   it 'handles the <sub> element' do
-    @etd = EtdModel::Etd.new
-
     props_ds = @etd.datastreams['properties']
     expect(props_ds.sub.to_a).to be_empty
   end
 
   it 'has an events datastream' do
-    @etd = EtdModel::Etd.new
     events = @etd.datastreams['events']
     expect(events).to be_a_kind_of Dor::EventsDS
   end
 
   it 'has an embargoMetadata datastream' do
-    @etd = EtdModel::Etd.new
     eds = @etd.datastreams['embargoMetadata']
     expect(eds).to be_a_kind_of Dor::EmbargoMetadataDS
   end
