2146cb6e8a69913f1b52c45db952894414b46d17
Joe Atzberger <ohiocore@gmail.com>
2016-01-21 12:15:35 -0800

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,18 +2,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
-
-# require 'rubygems'
 require 'etd_model'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
 end
 
-def class_exists?(class_name)
-  klass = Module.const_get(class_name)
-  return klass.is_a?(Class)
-rescue NameError
-  return false
-end
-
-Rails = Object.new unless class_exists? 'Rails'
